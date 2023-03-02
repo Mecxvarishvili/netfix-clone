9d3b1f055d0bdbe7b3b5df3f9257e2c3700956b5
Mecxvarishvili <vmecxvarishvili@gmail.com>
2023-03-02 15:45:46 +0400

    styling home pagev2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Image from "react-bootstrap/Image"
 import Form from "react-bootstrap/Form"
@@ -7,6 +7,17 @@ import page from './page';
 import { Link } from "react-router-dom"
 
 const HomePage = () => {
+    const contRef = useRef<HTMLDivElement | any >(null)
+    const [ contHeight, setContHeight ] = useState()
+
+    useEffect(() => {
+        if (!contRef.current) return; // wait for the elementRef to be available
+        const resizeObserver = new ResizeObserver(() => {
+            setContHeight(contRef?.current?.offsetHeight+ 100)
+        });
+        resizeObserver.observe(contRef.current);
+        return () => resizeObserver.disconnect();
+    }, [])
     return (
         <div className="hm-page text-white">
             <header className="position-relative pt-4" >
@@ -19,23 +30,25 @@ const HomePage = () => {
                 </div>
             </header>
             <div>
-                <div className="position-relative py-5 px-5p">
-                    <div style={{zIndex: -1, height: "500px"}} className="position-absolute top-0 start-0">
-                        <Image className='w-auto h-100' src={require("../images/background-image.jpg")} />
+                <div ref={contRef} className="position-relative py-5 px-5p border-bottom border-5 border-primary">
+                    <div style={{zIndex: -1, top: -90, height: `${contHeight}px`}} className="position-absolute start-0 w-100 bg-black">
+                        <Image className='w-100 h-100 position-relative object-fit-cover opacity-50' src={require("../images/background-image2.jpg")} />
                     </div>
-                    <div>
-                        <div>Unlimited movies, TV shows, and more.</div>
-                        <div>Watch anywhere. Cancel anytime.</div>
+                    <div className="main-box m-auto py-4 py-sm-5" >
+                        <div className="text-center fw-medium fs-xs-175 fs-sm-3125 fs-xxl-4 pt-md-5 lh-1 mx-5" >Unlimited movies, TV shows, and more.</div>
+                        <div className="text-center my-3 fs-xs-1125 fs-sm-1625">Watch anywhere. Cancel anytime.</div>
                         <Form className='' >
-                            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                            <InputGroup className="row m-auto" >
-                                <Form.Control className="col-2" size="lg" />
-                                <Button className="col-4" size="lg" >Get Started</Button>
-                            </InputGroup>   
+                            <div className="text-center pb-2 pb-sm-3 fs-xs-1125  fs-lg-12 px-5" >Ready to watch? Enter your email to create or restart your membership.</div>
+                            <div className="row justify-content-center" >
+                                <input className="input-cont col-lg-9"/>
+                                <Button className="button-cont fs-xs- fs-lg-3 bg-danger border-0 col-lg-3" >get Started</Button>
+                            </div>
                         </Form>
                     </div>
                 </div>
-                <div></div>
+                <div className="position-relative py-5 px-5p bg-black">
+
+                </div>
                 <div></div>
                 <div></div>
             </div>
