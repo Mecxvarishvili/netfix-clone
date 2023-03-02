aad46bb5d27163430a1f6561ab4a4b2f20785e1f
Mecxvarishvili <vmecxvarishvili@gmail.com>
2023-03-02 22:16:46 +0400

    Finished Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,7 +24,7 @@ const HomePage = () => {
                 <div className="d-flex justify-content-between mx-2 mx-sm-4 mx-lg-5">
                     <Image className='menu-logo' src={require("../images/Logonetflix.png")}/>
                     <div>
-                        <Link className="btn btn-danger btn-sm border-0 fs-sm-6 px-sm-3" to={page.LOGIN} >Sign In</Link>
+                        <Link className="btn btn-danger btn-sm border-0 fs-sm-1 fs-xs-09 px-sm-3" to={page.LOGIN} >Sign In</Link>
                     </div>
 
                 </div>
@@ -33,15 +33,16 @@ const HomePage = () => {
                 <div ref={contRef} className="position-relative py-5 px-5p border-bottom border-5 border-primary">
                     <div style={{zIndex: -1, top: -90, height: `${contHeight}px`}} className="position-absolute start-0 w-100 bg-black">
                         <Image className='w-100 h-100 position-relative object-fit-cover opacity-50' src={require("../images/background-image2.jpg")} />
+                        <div className='b-opacity position-absolute top-0 start-0 w-100 h-100'></div>
                     </div>
-                    <div className="main-box m-auto py-4 py-sm-5" >
-                        <div className="text-center fw-medium fs-xs-175 fs-sm-3125 fs-xxl-4 pt-md-5 lh-1 mx-5" >Unlimited movies, TV shows, and more.</div>
+                    <div className="main-box m-auto py-4 py-sm-5 mt-md-5" >
+                        <div className="text-center fw-medium fs-xs-175 fs-sm-3125 fs-xxl-4 pt-md-4 lh-1 mx-5" >Unlimited movies, TV shows, and more.</div>
                         <div className="text-center my-3 fs-xs-1125 fs-sm-1625">Watch anywhere. Cancel anytime.</div>
-                        <Form className='' >
+                        <Form className='mb-md-5' >
                             <div className="text-center pb-2 pb-sm-3 fs-xs-1125  fs-lg-12 px-5" >Ready to watch? Enter your email to create or restart your membership.</div>
-                            <div className="row justify-content-center" >
-                                <input className="input-cont col-lg-9"/>
-                                <Button className="button-cont fs-xs- fs-lg-3 bg-danger border-0 col-lg-3" >get Started</Button>
+                            <div className="row justify-content-center mx-2 gy-3" >
+                                <input className="input-cont col-8-10 border-0 mx-5 mx-lg-0"/>
+                                <Button className="button-cont bg-danger border-0 col-lg-4 w-100 rounded-0 mx-5 mx-lg-0 fs-lg-1625 fs-xxl-175" >Get Started</Button>
                             </div>
                         </Form>
                     </div>
