77e49d527bcdf2c24dfcea1b6a1adff988d298f3
Mecxvarishvili <vmecxvarishvili@gmail.com>
2023-03-07 22:00:05 +0400

    install formik

---
+++ src/components/InputPlacement.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
+type props = {
+  type: string,
+  formik: any
+}
+
+const InputPlacement = ({type, formik}: props) => {
+    return (
+      <div className="mb-3" >
+        <InputGroup className='bg-dark rounded rounded-1' >
+          <FloatingLabel 
+            controlId={type}
+            label="Email address"
+            className='position-relative'
+          >
+            <Form.Control
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values[type]}
+              className='bg-dark rounded rounded-1 border-0 text-white' 
+              name={type} 
+              type={type}
+              placeholder={type}
+            />
+            {formik.touched[type] && formik.errors[type] && <div className='position-absolute bottom-0 start-0 border-bottom border-2 border-warning rounded-bottom rounded-1 w-100' style={{height: "6px"}} ></div>}
+          </FloatingLabel>
+        </InputGroup>
+        <Form.Text>
+          {formik.touched[type] && formik.errors[type]? (
+            <div className="text-warning mt-1 font-13">{formik.errors[type]}</div>
+          ) : null}
+        </Form.Text>
+      </div>
+    );
+};
+
+export default InputPlacement;
--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -8,8 +8,34 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Link } from "react-router-dom"
 import page from './page';
+import { object, Asserts, string } from 'yup';
+import { Form as FormF, Formik, FormikHelpers, useFormik } from 'formik';
+import InputPlacement from '../components/InputPlacement';
 
 const LogInPage = () => {
+    interface Values  {
+        login: string,
+        password: string
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: object({
+            email: string()
+                .required("Please enter a valid email."),
+            password: string()
+                .min(8, "Your password must contain between 8 and 60 characters.")
+                .max(40, "Your password must contain between 8 and 60 characters.")
+                .required("Your password must contain between 4 and 60 characters.")
+        }),
+        onSubmit: value => {
+            alert("ss")
+        }
+    })
+
     return (
         <div className="text-secondary lg-page position-relative min-vh-100 bg-black" style={{zIndex: 0}}>
             <div className=" d-none d-md-block login-background position-absolute w-100 h-100 min-vh-100 overflow-hidden opacity-50" style={{zIndex: -1}}>
@@ -23,19 +49,40 @@ const LogInPage = () => {
             <div className="form-cont px-4 p-md-5 mx-auto bg-black bg-opacity-75 rounded" >
                 <div className="in-form-cont px-2 p-md-3 py-4">
                     <div className="fs-1 fw-bold text-white mb-4" >Sign In</div>
-                    <Form>
-                        <InputGroup size="lg">
-                            <Form.Control className="mb-3 bg-dark text-secondary border-0 rounded-1" type="email" />
-                        </InputGroup>
-                        <InputGroup size="lg">
-                            <Form.Control className="mb-3 bg-dark text-secondary border-0 rounded-1" type="password" />
-                        </InputGroup>
-                        <Button className="fs-6 fw-bolder mt-4 mb-3 w-100 rounded-1" variant="danger" size="lg" type="submit">Sign In</Button>
-                        <InputGroup className="d-flex justify-content-between" >
-                            <Form.Check label="Remember me" />
-                            <div>Need help?</div>
-                        </InputGroup>
-                    </Form>
+                        <Form onSubmit={formik.handleSubmit}>
+                            <InputPlacement type={"email"} formik={formik}/>
+                            <InputPlacement type={"password"} formik={formik}/>
+                            <Button className="fs-6 fw-bolder mt-4 mb-3 w-100 rounded-1" variant="danger" size="lg" type="submit">Sign In</Button>
+                            <InputGroup className="d-flex justify-content-between" >
+                                <Form.Check label="Remember me" />
+                                <div>Need help?</div>
+                            </InputGroup>
+                            {/* 
+                            <InputGroup size="lg">
+                                <Form.Control
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.login}
+                                    className="mb-3 bg-dark text-secondary border-0 rounded-1" name="login" type="email" />
+                                <Form.Text className="text-danger">
+                                  {formik.touched.login && formik.errors.login ? (
+                                    <div className="text-danger">{formik.errors.login}</div>
+                                  ) : null}
+                                </Form.Text>
+                            </InputGroup>
+                            <InputGroup size="lg">
+                                <Form.Control
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                    className="mb-3 bg-dark text-secondary border-0 rounded-1" name="password" type="password" />
+                                <Form.Text className="text-danger ">
+                                  {formik.touched.password && formik.errors.password ? (
+                                    <div className="text-danger">{formik.errors.password}</div>
+                                  ) : null}
+                                </Form.Text>
+                            </InputGroup> */}
+                        </Form>
                     <div className='mt-2 mt-sm-4 mt-md-5' >New to Netflix? <Link className='text-white text-decoration-none' to={page.SIGNUP} >SIgn up now</Link>.</div>
                     <div className="font-13 mt-3" >This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="text-info" >Learnm more.</span></div>
                 </div>
