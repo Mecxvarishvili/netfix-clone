ca4c628e6b975a70438250fa4d263eb8632f8376
Mecxvarishvili <vmecxvarishvili@gmail.com>
2023-04-07 23:48:25 +0400

    working on profile select

--- src/pages/browse/BrowseRoutes.tsx
+++ src/pages/browse/BrowseRoutes.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import page from '../page';
 import ModalPage from '../modal/ModalPage';
 import BrowsePage from './BrowsePage';
+import ProfileSelect from './ProfileSelect';
 
 const BrowseRoutes = () => {
     return (
+        false ?
         <div>
             <Routes>
                 <Route path={page.MODAL} element={<ModalPage />} />
             </Routes>
             <BrowsePage />
         </div>
+        : <ProfileSelect />
     );
 };
 
